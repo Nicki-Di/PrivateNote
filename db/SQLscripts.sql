6a5742688c2c38e9346d240b8d5fc7204d8b4403
Nicky <fatemesahraee2000@gmail.com>
2022-06-29 22:59:33 +0430

    Initial commit

---
+++ db/SQLscripts.sql
@@ -0,0 +1,4 @@
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+ALTER DATABASE PrivateNote CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SET GLOBAL max_allowed_packet = 1024 * 1024 * 256;
+
